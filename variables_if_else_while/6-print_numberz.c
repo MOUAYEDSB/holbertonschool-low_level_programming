b318125771ce8694d6fd0060a9a964416232c4c3
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-02-23 01:03:46 -0800

    ok

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n = 22;
+
+	while (n  <= 32)
+{
+		putchar(n);
+		n++;
+}
+	putchar ('\n');
+	return (0);
+}
