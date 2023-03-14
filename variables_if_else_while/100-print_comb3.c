65613f1a0ba94e6ed10976a5306ed286fb3ac97b
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-03-14 13:16:30 -0700

    comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 48; b <= 57; b++)
+			if (a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if ((a != 56) || (b != 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+	}
+			}
+	putchar('\n');
+	return (0);
+}
