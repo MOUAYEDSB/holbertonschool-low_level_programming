ee79e85e743b39f6d1137a347467586c47277edb
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-05 12:42:28 +0200

    all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,31 +2,37 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-typedef struct
-{
-	char letter;
-	void (*func)(va_list);
-} datatype;
-
-/* function to print a char */
+/**
+ * print_char - prints char
+ * @valist: valist
+ */
 void print_char(va_list valist)
 {
 	printf("%c", va_arg(valist, int));
 }
 
-/* function to print an int */
+/**
+ * print_int - prints int
+ * @valist: valist
+ */
 void print_int(va_list valist)
 {
 	printf("%d", va_arg(valist, int));
 }
 
-/* function to print a float */
+/**
+ * print_float - prints float
+ * @valist: valist
+ */
 void print_float(va_list valist)
 {
 	printf("%f", va_arg(valist, double));
 }
 
-/* function to print a string */
+/**
+ * print_string - prints string
+ * @valist: valist
+ */
 void print_string(va_list valist)
 {
 	char *s;
@@ -41,28 +47,25 @@ void print_string(va_list valist)
 	printf("%s", s);
 }
 
-/* function to print varying input of ints, chars, floats, and strings */
+/**
+ * print_all - print varying input of ints, chars, floats, and strings
+ * @format: an array of chars signifying which data type to print
+ */
 void print_all(const char *const format, ...)
 {
-	char *separator = "\n";
+	char *separator = "";
 	int i, j = 0;
 	va_list valist;
 
-	datatype choice[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}};
+	datatype choice[] = {{'c', print_char},
+						 {'i', print_int},
+						 {'f', print_float},
+						 {'s', print_string},
+						 {'\0', NULL}};
 
-	if (format == NULL)
-	{
-		printf("\n");
-		return;
-	}
-
+	/* iterate format; if datatype matched, access function via struct */
 	va_start(valist, format);
-	while (format[j] != '\0')
+	while (format != NULL && format[j] != '\0')
 	{
 		i = 0;
 		while (choice[i].letter != '\0')
@@ -70,8 +73,8 @@ void print_all(const char *const format, ...)
 			if (choice[i].letter == format[j])
 			{
 				printf("%s", separator);
-				choice[i].func(valist);
-				separator = ",\n ";
+				choice[i].func(valist); /*access va_arg later*/
+				separator = ", ";
 			}
 			i++;
 		}
