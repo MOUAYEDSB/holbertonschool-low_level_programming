d8779d17bc40b4a87df92aaa9bc6a005b7aa062c
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-05 14:32:04 +0200

    all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,85 +1,56 @@
-#include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdarg.h>
 
-/**
- * print_char - prints char
- * @valist: valist
- */
-void print_char(va_list valist)
-{
-	printf("%c", va_arg(valist, int));
-}
-
-/**
- * print_int - prints int
- * @valist: valist
- */
-void print_int(va_list valist)
-{
-	printf("%d", va_arg(valist, int));
-}
-
-/**
- * print_float - prints float
- * @valist: valist
- */
-void print_float(va_list valist)
-{
-	printf("%f", va_arg(valist, double));
-}
-
-/**
- * print_string - prints string
- * @valist: valist
- */
-void print_string(va_list valist)
-{
-	char *s;
-
-	s = va_arg(valist, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-
-/**
- * print_all - print varying input of ints, chars, floats, and strings
- * @format: an array of chars signifying which data type to print
- */
 void print_all(const char *const format, ...)
 {
-	char *separator = "";
-	int i, j = 0;
-	va_list valist;
-
-	datatype choice[] = {{'c', print_char},
-						 {'i', print_int},
-						 {'f', print_float},
-						 {'s', print_string},
-						 {'\0', NULL}};
-
-	/* iterate format; if datatype matched, access function via struct */
-	va_start(valist, format);
-	while (format != NULL && format[j] != '\0')
+	char c;
+	int i;
+	float f;
+	char *s;
+	int printed = 0;
+	const char *format_ptr = format;
+	va_list args;
+	va_start(args, format);
+	while (*format_ptr)
 	{
-		i = 0;
-		while (choice[i].letter != '\0')
+		if (*format_ptr == 'c')
 		{
-			if (choice[i].letter == format[j])
-			{
-				printf("%s", separator);
-				choice[i].func(valist); /*access va_arg later*/
-				separator = ", ";
-			}
-			i++;
+			c = va_arg(args, int);
+			printf("%c", c);
+			printed = 1;
 		}
-		j++;
+		else if (*format_ptr == 'i')
+		{
+			i = va_arg(args, int);
+			printf("%d", i);
+			printed = 1;
+		}
+		else if (*format_ptr == 'f')
+		{
+			f = va_arg(args, double);
+			printf("%f", f);
+			printed = 1;
+		}
+		else if (*format_ptr == 's')
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", s);
+			}
+			printed = 1;
+		}
+		if (*(format_ptr + 1) && printed)
+		{
+			printf(", ");
+			printed = 0;
+		}
+		format_ptr++;
 	}
-	va_end(valist);
 	printf("\n");
+	va_end(args);
 }
