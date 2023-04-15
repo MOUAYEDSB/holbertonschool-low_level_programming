de817f8ab4a9e3ca7a4d7d8c49f8a55a000561f5
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-14 15:12:43 -1200

    flip

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -15,7 +15,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	difference = n ^ m;
 
 	/* keep shifting difference to right and tallying the ones up */
+
 	do
+
 	{
 		diff_bits += (difference & 1);
 		difference >>= 1;
